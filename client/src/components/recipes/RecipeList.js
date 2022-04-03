1b99a0f8573be37da057e19ccd8938ac45a51bf2
chall6753 <chall70101@gmail.com>
2022-04-03 11:26:28 -0600

    added filter for recipes

--- client/src/components/recipes/RecipeList.js
+++ client/src/components/recipes/RecipeList.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import RecipeCard from './RecipeCard'
 
 function RecipeList({recipes}) {
+  const [filteredRecipes, setFilteredRecipes] = useState(recipes)
 
-console.log(recipes)
+  function filterRecipes(e){
+      if (e.target.value == ''){
+        setFilteredRecipes(recipes)
+      } 
+      else{
+        let r = recipes.filter(recipe => e.target.value.toLowerCase() == recipe.name.toLowerCase())
+      
+      setFilteredRecipes(r)
+      }
+      
+      
+  }
   return (
     <div >
       <h1>Recipe List</h1>
+      <label>Search for a recipe</label>
+      <input type='text' onChange={filterRecipes}></input>
         
       <ul>
-        {recipes.map(recipe => {
+        {filteredRecipes.map(recipe => {
           return <RecipeCard recipe={recipe}/> 
         })
         }
